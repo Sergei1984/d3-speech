668fc8fc91fac68ec02036461a7cfd9eaf513fa8
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2019-05-12 22:51:43 +0300

    Add some basic code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./App.less";
+
+export function App() {
+    return (
+        <div className="App">
+            <div className="App__header">Header </div>
+            <div className="App__content">Content </div>
+        </div>
+    );
+}
