38e36c247fea24efb6130c6eae0987bfc494cbb1
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2019-05-12 23:00:56 +0300

    Some basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import "./App.less";
 export function App() {
     return (
         <div className="App">
-            <div className="App__header">Header </div>
+            <div className="App__header">D3 ❤️ React</div>
+            <div className="App__menu">Menu</div>
             <div className="App__content">Content </div>
         </div>
     );
