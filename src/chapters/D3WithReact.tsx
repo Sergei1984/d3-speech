94ed11ef1b211fa966ee16568e090d91be3e78ea
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2019-05-17 01:23:21 +0300

    Get rid of markdown

--- src/chapters/D3WithReact.tsx
+++ src/chapters/D3WithReact.tsx
@@ -2,41 +2,47 @@ import * as React from "react";
 import { RouteChildrenProps } from "react-router";
 import { Chapter } from "./Chapter";
 
-import { Markdown } from "../components";
-
 export function D3WithReact(props: RouteChildrenProps<{ tab: string }>) {
     return (
         <Chapter {...props} title="D3 + React">
-            <Markdown>
-                {`
-## Use D3 for:
-* Interpolating values to document (chart) dimensions
-* Generating shapes for paths
-
-
-## Use React for:
-
-* Building DOM of __SVG__
-* Update DOM
-* Handle events
-
-## Pros:
-* Declarative, functional and straightforward document structure (vs contrintuitive D3 DOM building methods)
-* Reusable components for chart parts: axis, lines etc.
-* Smaller application size: don't inlcudes unnecessary D3 parts
-
-## Cons:
-* Animations are not supported
-* D3 event handling is not supported
-* For some complex charts may need direct DOM mutations
-
-
-## Alternative
-
-* [react-faux-dom](https://github.com/Olical/react-faux-dom) - mutable in-memory DOM emulation that can be rendered into React component tree
-
-`}
-            </Markdown>
+            <h2>Use D3 for:</h2>
+            <ul>
+                <li>Interpolating values to document (chart) dimensions </li>
+                <li>Generating shapes for paths</li>
+            </ul>
+            <h2>Use React for:</h2>
+            <ul>
+                <li>Building DOM of SVG</li>
+                <li>Update DOM</li>
+                <li>Handle events</li>
+            </ul>
+            <h2>Pros:</h2>
+            <ul>
+                <li>
+                    Declarative, functional and straightforward document
+                    structure (vs contrintuitive D3 DOM building methods)
+                </li>
+                <li>Reusable components for chart parts: axis, lines etc.</li>
+                <li>
+                    Smaller application size: don't inlcudes unnecessary D3
+                    parts
+                </li>
+            </ul>
+            <h2>Cons:</h2>
+            <ul>
+                <li>Animations are not supported</li>
+                <li>D3 event handling is not supported</li>
+                <li>For some complex charts may need direct DOM mutations</li>
+            </ul>
+            <h2>Alternative:</h2>
+            <p>
+                <a href="https://github.com/Olical/react-faux-dom">
+                    react-faux-dom
+                </a>{" "}
+                - mutable in-memory DOM emulation that can be rendered into
+                React component tree.
+            </p>
+            <p>Use it if you need to use imperative D3 methods with React.</p>
         </Chapter>
     );
 }
--- src/chapters/Intro.tsx
+++ src/chapters/Intro.tsx
@@ -2,34 +2,48 @@ import * as React from "react";
 import { RouteChildrenProps } from "react-router";
 import { Chapter } from "./Chapter";
 
-import { Markdown } from "../components";
-
 export function Intro(props: RouteChildrenProps<{ tab: string }>) {
     return (
         <Chapter {...props} title="Intro">
-            <Markdown>
-                {`
-## What is D3
-
-* D3 stands for _Data Driven Documents_
-* D3 is a library for create _documents_ with representation of data.
-* D3 provides a set of _primitives_ which allows to build any chart
-* D3 requires more manual work but allows to create any type of charts
-
-## SVG
-
-* In the speech we will consider only D3 with __SVG__
-* D3 also is able to work with _canvas_ or can use HTML for simple charts (for example bar charts)
-
-
-## D3 Parts
-
-* D3 consists of many parts, most of them we will not use and will not consider
-* D3 includes code for _building_ __SVG__ document DOM. We will use __React__ instead
-
-
-[See how to use D3 with React](/d3-with-react/md)`}
-            </Markdown>
+            <h2>What is D3</h2>
+            <ul>
+                <li>
+                    D3 stands for <i>Data Driven Document</i>
+                </li>
+                <li>
+                    D3 is a library for create <i>documents</i> with
+                    representation of data.
+                </li>
+                <li>
+                    D3 provides a set of <i>primitives</i> which allows to build
+                    any chart
+                </li>
+                <li>
+                    D3 requires more manual work but allows to create any type
+                    of charts
+                </li>
+            </ul>
+            <h2>SVG</h2>
+            <ul>
+                <li>In the speech we will consider only D3 with SVG</li>
+                <li>
+                    D3 also is able to work with canvas or can use HTML for
+                    simple charts (for example bar charts)
+                </li>
+            </ul>
+            <h2>D3 parts</h2>
+            <ul>
+                <li>
+                    D3 consists of many parts, most of them we will not use and
+                    will not consider since they are not usable with React of
+                    pretty specialized
+                </li>
+                <li>
+                    D3 includes code for _building SVG document DOM. We will use
+                    React instead
+                </li>
+            </ul>
+            <a href="/d3-with-react">How to use D3 with React</a>
         </Chapter>
     );
 }
