657ba98df71ae14c59fa119f9406c04c3f81c31d
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2019-05-23 20:25:17 +0300

    +++

--- src/chapters/C6_ChartBuildingAlgorithm.tsx
+++ src/chapters/C6_ChartBuildingAlgorithm.tsx
@@ -92,6 +92,98 @@ function LineChart() {
             </p>
             <p>
                 There are few types of <code>scales</code>:
+                <ul>
+                    <li>
+                        <a href="https://github.com/d3/d3-scale#linear-scales">
+                            <code>scaleLinear</code>
+                        </a>{" "}
+                        - allows to map continous values in <i>domain</i> (ie in
+                        data) to continuos <i>range</i> of target dimension (for
+                        example, in pixel position on the chart)
+                    </li>
+                    <li>
+                        <a href="https://github.com/d3/d3-scale#time-scales">
+                            <code>scaleTime</code>
+                        </a>{" "}
+                        - same as <code>scaleLinear</code> but for time (
+                        <code>Date</code>) data
+                    </li>
+                    <li>
+                        <a href="https://github.com/d3/d3-scale#ordinal-scales">
+                            <code>scaleOrdinal</code>
+                        </a>{" "}
+                        - allows to map one list of values to another list. This
+                        for example could be a mapping of day of weeks to colors
+                        etc.
+                    </li>
+                    <li>
+                        <a href="https://github.com/d3/d3-scale#point-scales">
+                            <code>scalePoint</code>
+                        </a>{" "}
+                        - maps a list of values to list of points uniformly
+                        distributed points inside a given range. This is useful
+                        if you need to draw bar chart (but for bars it is better
+                        to use{" "}
+                        <a href="https://github.com/d3/d3-scale#scaleBand">
+                            <code>scaleBand</code>
+                        </a>{" "}
+                        which allows to specify a band width, paddings etc.)
+                    </li>
+                </ul>
+            </p>
+            <h4>Scale configuration</h4>
+            <p>
+                Each scale is configured in similar way. Starting from importing
+                scale from D3 package, then specify domain, range. Result would
+                be a function which actually performs a mapping.
+            </p>
+            <CodeBlock language="typescript">
+                {`
+import { scaleLinear } from "d3";
+
+const amountScale = scaleLinear()
+                        .domain([0, 1500000]) // Domain is range of values inside a data
+                        .range([0, HEIGHT]);  // Range is min and max values domain would be fit into.
+                                              // Usually that's a bounds of chart
+console.log(amountScale(10000)); // Would output HEIGHT * 10000 / 1500000
+                `}
+            </CodeBlock>
+            <h5>
+                <code>Domain</code>
+            </h5>
+            <p>
+                <i>Domain</i> in D3 is a range of values inside the data. For
+                example, if data is like that:
+            </p>
+            <table>
+                <tr>
+                    <td>Year</td>
+                    <td>2015</td>
+                    <td>2016</td>
+                    <td>2017</td>
+                    <td>2018</td>
+                </tr>
+                <tr>
+                    <td>Profit</td>
+                    <td>50 000</td>
+                    <td>67 000</td>
+                    <td>42 000</td>
+                    <td>112 000</td>
+                </tr>
+            </table>
+            <p>
+                then domain of <b>Profit</b> could be either{" "}
+                <code>[0, 112000]</code> or <code>[42000, 112000]</code>, or
+                even <code>[10000, 200000]</code>. Depending on that the line on
+                chart will have different zoom and lower and upper bounds.
+            </p>
+            <h5>
+                <code>Range</code>
+            </h5>
+            <p>
+                <i>Range</i> determine an target range (usually width or height
+                of the chart) in which values from <i>domain</i> would be
+                mapped.
             </p>
         </Chapter>
     );
